c3f1bdbcd3246ad2972d6ef4c6dc32434ad2eed5
Daniel-Egorov <daniel.s.egorov@gmail.com>
2022-01-18 22:49:53 -0800

    create email filtering website

---
+++ script.js
@@ -0,0 +1,38 @@
+const file = document.getElementById("file");
+const reader = new FileReader();
+const emailRegex = /[\w\.-]+@[\w\.-]+/g;
+
+file.addEventListener("change", (event) => {
+  let userFile = file.files[0];
+  if (!userFile) return;
+  let read;
+  let filtered;
+  reader.onload = function () {
+    let fileName = userFile.name.replace(".txt", "");
+    fileName = fileName + "_FILTER";
+    read = reader.result;
+    filtered = filterStr(read).replace(" ", "");
+    console.log(read);
+    console.log(filtered);
+    download(fileName, filtered);
+  };
+
+  reader.readAsText(userFile);
+});
+
+function filterStr(str) {
+  return str.match(emailRegex).join("\n");
+}
+
+function download(file, text) {
+  var element = document.createElement("a");
+  element.setAttribute("href", "data:text/plain;charset=utf-8, " + encodeURIComponent(text));
+  element.setAttribute("download", file);
+
+  document.body.appendChild(element);
+
+  //onClick property
+  element.click();
+
+  document.body.removeChild(element);
+}
